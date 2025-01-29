70df161eeeccf8ebdd6cbd550304b47309f884ad
rfaria-dev <roger.faria.dev@outlook.com>
2025-01-29 01:57:55 -0300

    feat: add MongoDB connection and dotenv configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Hello, world!');
+import chalk from "chalk";
+import express from "express";
+import dotenv from "dotenv";
+import { connectToDataBase } from "./database/mongoose.database.js";
+
+dotenv.config();
+
+const app = express();
+
+connectToDataBase();
+
+app.get("/tasks", (req, res) => {
+    const tasks = [
+        {
+            description: "Estudar",
+            isCompleted: false,
+        },
+        {
+            description: "Ler um livro",
+            isCompleted: false,
+        },
+    ];
+    res.status(200).send(tasks);
+});
+
+app.listen(8000, () => {
+    console.log(chalk.cyanBright("Server is running on port 8000!"));
+});
