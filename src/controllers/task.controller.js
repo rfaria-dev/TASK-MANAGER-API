3273f9f197b65a3554eeeb23df9c13674318e7ca
Roger-Faria <rfaria2@lenovo.com>
2025-01-30 02:04:03 -0300

    feat: move getTasksById, create task, updateTask, deleteTasks endpoint to the controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -13,6 +13,84 @@ class TaskController {
 			this.res.status(500).send(err.message);
 		}
 	};
+	getTasksById = async () => {
+		try {
+			const taskId = this.req.params.id;
+			const task = await taskModel.findById(taskId);
+
+			if (!task) {
+				return this.res.status(404).send("Task not found!");
+			}
+
+			this.res.status(200).send(task);
+		} catch (err) {
+			this.res.status(500).send(err.message);
+		}
+	};
+	createTask = async () => {
+		try {
+			const newTask = new taskModel(this.req.body);
+			await newTask.save();
+
+			this.res.status(201).send(newTask);
+		} catch (err) {
+			this.res.status(500).send(err.message);
+		}
+	};
+	deleteTasks = async () => {
+		try {
+			const taskId = this.req.params.id;
+			const currentTask = await taskModel.findById(taskId);
+
+			if (!currentTask) {
+				return this.res.status(404).send("Task not found!");
+			}
+
+			const taskToBeDeleted = await taskModel.findByIdAndDelete(taskId);
+			this.res.status(200).send(taskToBeDeleted);
+		} catch (err) {
+			this.res.status(500).send(err.message);
+		}
+	};
+	updateTask = async () => {
+		try {
+			const taskId = this.req.params.id;
+			const taskData = this.req.body;
+			const allowedUpdates = ["isCompleted"];
+			const requestedUpdates = Object.keys(taskData);
+
+			// Filter allowed updates
+			const updatesToApply = {};
+			for (const update of requestedUpdates) {
+				if (allowedUpdates.includes(update)) {
+					updatesToApply[update] = taskData[update];
+				} else {
+					return this.res
+						.status(400)
+						.send(
+							"Invalid update: One or more editable fields are not allowed to be updated"
+						);
+				}
+			}
+			// Update the document in the database
+			const updatedTask = await taskModel.findByIdAndUpdate(
+				taskId,
+				updatesToApply,
+				{ new: true, runValidators: true } // Return the updated document and run validations
+			);
+
+			// Check if the task exists
+			if (!updatedTask) {
+				return this.res.status(404).send("Task not found");
+			}
+
+			// Return the updated task
+			return this.res.status(200).send(updatedTask);
+		} catch (err) {
+			// Handle errors
+			return res.status(500).send(err.message);
+		}
+	};
 }
 
 export { TaskController };
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { taskModel } from "../models/task.model.js";
 import { TaskController } from "../controllers/task.controller.js";
 
 const router = express.Router();
@@ -9,85 +8,19 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-	try {
-		const taskId = req.params.id;
-		const task = await taskModel.findById(taskId);
-
-		if (!task) {
-			return res.status(404).send("Task not found!");
-		}
-
-		res.status(200).send(task);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
+	return new TaskController(req, res).getTasksById();
 });
 
 router.post("/", async (req, res) => {
-	try {
-		const newTask = new taskModel(req.body);
-		await newTask.save();
-
-		res.status(201).send(newTask);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
+	return new TaskController(req, res).createTask();
 });
 
 router.delete("/:id", async (req, res) => {
-	try {
-		const taskId = req.params.id;
-		const currentTask = await taskModel.findById(taskId);
-
-		if (!currentTask) {
-			return res.status(404).send("Task not found!");
-		}
-
-		const taskToBeDeleted = await taskModel.findByIdAndDelete(taskId);
-		res.status(200).send(taskToBeDeleted);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
+	return new TaskController(req, res).deleteTasks();
 });
 
 router.patch("/:id", async (req, res) => {
-	try {
-		const taskId = req.params.id;
-		const taskData = req.body;
-		const allowedUpdates = ["isCompleted"];
-		const requestedUpdates = Object.keys(taskData);
-
-		// Filter allowed updates
-		const updatesToApply = {};
-		for (const update of requestedUpdates) {
-			if (allowedUpdates.includes(update)) {
-				updatesToApply[update] = taskData[update];
-			} else {
-				return res
-					.status(400)
-					.send(
-						"Invalid update: One or more editable fields are not allowed to be updated"
-					);
-			}
-		}
-		// Update the document in the database
-		const updatedTask = await taskModel.findByIdAndUpdate(
-			taskId,
-			updatesToApply,
-			{ new: true, runValidators: true } // Return the updated document and run validations
-		);
-
-		// Check if the task exists
-		if (!updatedTask) {
-			return res.status(404).send("Task not found");
-		}
-
-		// Return the updated task
-		return res.status(200).send(updatedTask);
-	} catch (err) {
-		// Handle errors
-		return res.status(500).send(err.message);
-	}
+	return new TaskController(req, res).updateTask();
 });
 
 export { router as taskRouter };
